e45d91d4961715581ad395e43b68b01fce509698
Me <jeremiah.brock10@gmail.com>
2023-05-16 10:52:07 -0400

    Coded out most of the screens, added text, color, & images

--- SoCo/PartyView.swift
+++ SoCo/PartyView.swift
@@ -15,20 +15,31 @@ struct PartyView: View {
                 .frame(width: 334 , height: 342)
             Spacer()
             
-            Rectangle()
-                .frame(width: 334, height: 170)
-                .cornerRadius(15)
-            
+           Text("You’re at a house party but you don’t know anybody except the host. You’re feeling a bit nervous and unsure of what to do ")
+             //   .frame(width: 334, height: 170)
+                .background(Color("ColorS"))
+                .fontWeight(.medium)
+                //.cornerRadius(15)
+                .font(.title2)
+            Spacer()
             NavigationLink(destination: Answer1()) {
-                Rectangle()
-                    .frame(width: 330 , height: 35)
+                Text("Hang out with the host")
+                    .background(Color("ColorM"))
+                    .fontWeight(.medium)
+                   // .frame(width: 330 , height: 35)
                     .cornerRadius(15)
+                    
             }
-            Rectangle()
-            .frame(width: 330 , height: 35)
+            Text("Strike up a conversation with someone")
+                .background(Color("ColorM"))
+                .fontWeight(.medium)
+         //   .frame(width: 330 , height: 35)
             .cornerRadius(15)
-            Rectangle()
-            .frame(width: 330 , height: 35)
+            .padding()
+            Text("Leave the party")
+                .background(Color("ColorM"))
+                .fontWeight(.medium)
+           // .frame(width: 330 , height: 35)
             .cornerRadius(15)
         }
     }
